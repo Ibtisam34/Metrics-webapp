049f7ce2efb38fb2798d21f7c6b39d5d2598f0cf
unknown <Riyanagwl@gmail.com>
2023-01-18 18:44:15 +0300

    add testing

---
+++ src/components/__test__/page-test/weathercountries.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../../../redux/configureStore';
+import Countries from '../../../pages/homepage/weathercountries';
+
+describe('Testing Countries component', () => {
+  test('Testing the dom for success rendering elements on Countries component', () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <Countries />
+      </Provider>,
+    );
+    expect(getByTestId('countries-wrapper')).toBeInTheDocument();
+  });
+  test('Testing the dom for non existing elemente on the page after rendering', () => {
+    const { queryByTestId } = render(
+      <Provider store={store}>
+        <Countries />
+      </Provider>,
+    );
+    expect(queryByTestId('nothing')).toBeNull();
+  });
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+           <Countries />
+        </Provider>
+         
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+
+
+
+
+
+
+
+
